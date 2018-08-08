5244d76ab1411ac5311eaa037ae16e7fe42f1b93
giantsda <38990244+giantsda@users.noreply.github.com>
2018-08-07 22:39:09 -0600

    DONE: merge porous.c with random.c 2. add comments. 3. bugs fixed. TODO: improve it.

--- try.c
+++ try.c
@@ -1,109 +1,236 @@
+/*
+ ============================================================================
+ Name        : porous.c
+ Author      : Chen Shen
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
 #include "udf.h"
 
-int n=0;
-int number_of_cells;
-double eps=1e-8;
-double node_x[2],node_z[2]; // node_x stores the smallest and largest value of all node x component.
-int  *cell_status;
+// Global data
+int max_state = 3000;
+int number_filter_cells;
+double* filter_cell_status;
+double* current_add;
+int filter_ID = 6;
+double base_vr = 3.7822e+10;
+int allowed_hit_one_step = 5;
+Injection * injection; // I need to pass it to DEFINE_EXECUTE_AT_END
+double x0 = 0.07054139, y0 = 0.049, z0 = 0., r = 0.000985; /* defines the origin for random particle injection
+ check r value for finer mesh */
 
- DEFINE_EXECUTE_ON_LOADING(on_laoding, libname)
-{
-	number_of_cells=0;
-	Domain * domain=Get_Domain(1);  // 1 is for single phase
-	Thread * thread=Lookup_Thread(domain,8); // 
-	int zone_ID=	 THREAD_ID(thread);
-	real x[ND_ND];
-	cell_t c;
-	begin_c_loop(c,thread)
-		{
-			C_CENTROID(x,c,thread);
-			//	printf("cell: %d on node:%d \n",c,myid);
-			number_of_cells++;
-		}
-	end_c_loop(f,t);
-	printf("number_of_cells=%d on node:%d <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< \n",number_of_cells,myid);
-	cell_status=(int*)malloc(sizeof(int)*number_of_cells);
-	printf("memory is allocated from node %d\n",myid);
-	for (int i=0;i<number_of_cells;i++)
-	{
-		cell_status[i]=0;
-	}
-	fflush(stdout); 
-}
- 
-  DEFINE_PROFILE(change_vis_res,t,i)
-{
-	//printf("number_of_cells=%d \n",number_of_cells);
-     cell_t c;
-	 real x[ND_ND],a;
-	begin_c_loop(c,t)
-	{
-		if (x[0]<0)
-			a=1e9;
-		else
-			a=1;
-		F_PROFILE(c,t,i)=a;
-		int haha=C_NNODES(c,t);
-		 	}
-	end_c_loop(c,t)
-	fflush(stdout); 
-}    
+// Function prototype
+int
+Probablity (double p);
 
-DEFINE_DPM_SCALAR_UPDATE(dpm_udf,c,t,initialize,p)
+DEFINE_EXECUTE_ON_LOADING(Initialize, libname) 
 {
-	Domain * domain=Get_Domain(1);  // 1 is for single phase
-	Thread * filter_thread=Lookup_Thread(domain,8); // 
-	if(t==filter_thread)
-	{
-		printf("particle %d in cell: %d,\
-		position: %f,%f,%f, current time=%f on node %d\n",p->part_id, c,P_POS(p)[0],P_POS(p)[1],P_POS(p)[2],P_TIME(p),myid);
-		cell_status[c]++;
-		p->stream_index = -1; 
-		p->gvtp.n_aborted++; // once a particle reaches the filter, it is marked as aborted.
-	}
-	fflush(stdout); 
+	/* 1. calculate how many cells are in the filter each node. 
+	2. Allocate memory for filter_cell_status and current_add. 
+	3. initialize filter_cell_status. 
+	4. set seed for random number generator */
+  number_filter_cells = 0;
+  Domain * domain = Get_Domain (1);  // 1 is for single phase
+  Thread * thread = Lookup_Thread (domain, filter_ID); //
+  cell_t c;
+  begin_c_loop (c, thread)
+    {
+      number_filter_cells++;
+    }
+  end_c_loop (c, thread);
+  printf ("\n number_filter_cells=%d on node:%d <<<<<<<<<<<<<<<<<<<< \n",
+	  number_filter_cells, myid);
+  filter_cell_status = (double*) malloc (sizeof(double) * number_filter_cells);
+  current_add = (double*) malloc (sizeof(double) * number_filter_cells);
+  for (int i = 0; i < number_filter_cells; i++) // initialize filter_cell_status
+    {
+      filter_cell_status[i] = 0.;
+    }
+  srand (time (0) + myid * 123);  // set seed for random number
+  fflush (stdout);
 }
 
-DEFINE_ON_DEMAND(print_cell_status)
+DEFINE_DPM_SCALAR_UPDATE(set_filter_cell_status, c, t, initialize, p) 
 {
-	//for (int i=0;i<number_of_cells;i++)
-		//printf("cell_status[%d]=%d; \n",i,cell_status[i]);
-	// writting cell_status file
-	//FILE *fp = NULL;
-	
-	if (!RP_HOST)
+	/* 1.set_filter_cell_status once a particle hits the filter*/
+  Domain * domain = Get_Domain (1);  // 1 is for single phase
+  Thread * filter_thread = Lookup_Thread (domain, filter_ID);
+  if (t == filter_thread) // Thread comparison. True if the particle is in the filter.
+    {
+      //printf("particle %d in cell: %d,position: %f,%f,%f, current time=%f on node %d\n",p->part_id, c,P_POS(p)[0],P_POS(p)[1],P_POS(p)[2],P_TIME(p),myid);
+      if (filter_cell_status[c] < max_state
+	  && current_add[c] < allowed_hit_one_step && N_TIME % 1 == 0) // The max allowed hit per cell, and max allowed hits per time
 	{
-	Domain * domain=Get_Domain(1);  // 1 is for single phase
-	Thread * thread=Lookup_Thread(domain,8); // 		
-	char filename[256];
-    strcpy (filename,"cell_status_node");
-	char snum[64];
-	sprintf(snum, "%d", myid);
-	strcat (filename,snum);
-	strcat (filename,".txt");
-	FILE* fp=fopen(filename,"w");
-	if (fp==NULL)
-		printf("ERROR: Unable to open %s\n",filename);
-	else
-		printf("writing local data to %s on node %d\n",filename,myid);
- // Loop over cells on filter
- 	real x[ND_ND];
-	cell_t c;
-	begin_c_loop(c,thread)
-		{
-			C_CENTROID(x,c,thread);
-			fprintf(fp,"%f,%f,%f,%d\n",x[0],x[1],x[2],cell_status[c]);
-		}
-	end_c_loop(f,t);
-	fclose(fp);
-	
-	
-	 
-	fflush(stdout); 
+	  filter_cell_status[c]++;
+	  current_add[c]++;
 	}
-
+      p->stream_index = -1;
+      p->gvtp.n_aborted++; // once a particle reaches the filter, it is marked as aborted.
+    }
+  fflush (stdout);
 }
 
+DEFINE_PROFILE(change_vis_res, t, i) 
+{
+	/* 1. change the cell_vr based on how many particles have hit the filter */
+  cell_t c;
+  real cell_vr;
+  begin_c_loop (c, t)
+    {
+      cell_vr = base_vr * (1 + filter_cell_status[c] / max_state * 10); //if the cell got max hit, the vr is 11 times of the base_vr
+      F_PROFILE (c, t, i) = cell_vr;
+    }
+  end_c_loop (c, t)
+  fflush (stdout);
+}
 
+DEFINE_DPM_INJECTION_INIT(random_injection, I) 
+{
+	/* 1. generate random position for injection. 2.Pass I for further use.*/
+	
+  // printf("Executed at time step %d from node %d \n",N_TIME,myid);
+  Particle *p;
+  injection = I;
+  loop (p, I->p_init)
+  /* Standard Fluent Looping Macro to get particle. >> p_init << for transient mode,p for SS */
+    {
+      double x = 2000., y = 2000., z = 2000.; // make sure it is out of the domain initially.
+      if (Probablity (0.9))  //80 percent particle is going to be deleted.
+	p->stream_index = -1;  // delete particle.
+      else 			// if not deleted, its position is regenerated.
+	{
+	  while (sqrt (pow (x - x0, 2) + pow (z - z0, 2)) >= r) // while it is out of the domain
+	    {
+	      x = x0
+		  + (double) rand () / (double) ((unsigned) RAND_MAX + 1) * 2
+		      * r - r;  // from -0.001 to 0.001
+	      y = y0
+		  + (double) rand () / (double) ((unsigned) RAND_MAX + 1) * 2
+		      * r * 0.2 - 0.2 * r; // vertical locations are allowed to move by 0.2r
+	      z = z0
+		  + (double) rand () / (double) ((unsigned) RAND_MAX + 1) * 2
+		      * r - r;
+	    }
+	  P_POS (p)[0] = x;
+	  P_POS (p)[1] = y;
+	  P_POS (p)[2] = z;
+	}
+    }
+  fflush (stdout);
+}
+
+DEFINE_EXECUTE_AT_END(set_UDS) 
+{
+	/*1.set UDS for postprocessing. UDS_1 for filter_cell_status and 
+	UDS_2 denotes it is in the filter or not. 
+	2. restore deleted particles. 
+	3. reinitialize current_add */
+  Domain * domain = Get_Domain (1);  // 1 is for single phase
+  Thread * thread = Lookup_Thread (domain, filter_ID); //
+  cell_t c;
+  begin_c_loop (c, thread)
+    {
+      C_UDSI (c, thread, 0) = filter_cell_status[c];
+      C_UDSI (c, thread, 1) = 1.;
+    }
+  end_c_loop (c, Thread);
+
+//  if (RP_HOST)
+//    printf ("\n>>> UDS_0 is set \n");
+
+  for (int i = 0; i < number_filter_cells; i++)
+    {
+      current_add[i] = 0;
+    }
+
+//  if (RP_HOST)
+//    printf (">>> current_add is clear to 0\n");
+
+  if (!RP_HOST) // have to add it because host node does not have data on it!
+    {
+      Particle *p;
+      loop (p, injection->p_init)
+	{
+	  p->stream_index = p->part_id; // recover deleted particles.I think it is OK for any position int.
+	}
+    }
+  fflush (stdout);
+}
+
+DEFINE_ON_DEMAND(fprint_cell_status) /* print cell status to files */
+{
+  if (!RP_HOST)
+    {
+      Domain * domain = Get_Domain (1);  // 1 is for single phase
+      Thread * thread = Lookup_Thread (domain, filter_ID); //
+      char filename[256];
+      strcpy (filename, "filter_cell_status_node_");
+      char snum[64];
+      sprintf (snum, "%d", myid);
+      strcat (filename, snum);
+      strcat (filename, ".txt");
+      FILE* fp = fopen (filename, "w");
+      if (fp == NULL)
+	printf ("ERROR: Unable to open %s\n", filename);
+      else
+	printf ("writing local data to %s on node %d\n", filename, myid);
+      // Loop over cells on filter
+      real x[ND_ND];
+      cell_t c;
+      begin_c_loop (c, thread)
+	{
+	  C_CENTROID (x, c, thread);
+	  fprintf (fp, "%f,%f,%f,%d\n", x[0], x[1], x[2],
+		   filter_cell_status[c]);
+	}
+      end_c_loop (c, thread);
+      fclose (fp);
+      fflush (stdout);
+    }
+}
+
+DEFINE_ON_DEMAND(random_number_generator) /* for debugging */
+{
+  /*	printf("current is %f,  time step=%d,   on node %d \n", CURRENT_TIME,N_TIME,myid);
+   //if(myid==0)
+   {
+   for(int i = 0; i<100; i++)
+   {
+   if(Probablity(0.1))
+   printf("1");
+   else
+   printf("0");
+   }
+   printf("\n");
+   }
+   */
+
+  if (!RP_HOST)
+    {
+      printf ("injection is >>> %p \n", injection);
+      Particle *p;
+
+      loop (p, injection->p)
+	{
+	  printf ("particle %d stream index=%d\n", p->part_id, p->stream_index);
+	  p->stream_index = p->part_id;
+	}
+    }
+
+  fflush (stdout);
+}
+
+int
+Probablity(double p)
+{
+  double random_number = (double) rand () / (double) ((unsigned) RAND_MAX + 1);
+  if (random_number < p)
+    return 1;
+  else
+    return 0;
+}
 
 
